c43cef2249a7f787b7a4f9868ddafa9503849b89
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-11-29 23:54:56 -0300

    Deploy test

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./pages/Home";
 import { EditImages } from "./pages/EditImages";
-import { NavBar } from "./components/Navbar"
-import { Footer } from "./components/Footer"
+import { NavBar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 function App() {
-  return (
-    <div className="">
-      <header className="my-3">
-       <NavBar />
-      </header>
-      <body>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/edit' element={<EditImages />}/>
-      </Routes>
-      </body>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
+	return (
+		<div className="">
+			<header className="my-3">
+				<NavBar />
+			</header>
+			<body>
+				<Routes>
+					<Route path="/meme-generator" element={<Home />} />
+					<Route path="/edit" element={<EditImages />} />
+				</Routes>
+			</body>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
 }
 
 export default App;
