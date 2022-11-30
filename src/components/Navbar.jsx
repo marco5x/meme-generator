b4b830401a55bd5005e32b44aecbec1a793dbcf9
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-11-30 00:09:03 -0300

    Generated link, component navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
         <Container>
             <Navbar expand="lg" variant="light" bg="light">
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand href="/meme-generator">
                         <div className="d-flex gap-4 align-items-center">
                             <img
                                 alt="meme"
