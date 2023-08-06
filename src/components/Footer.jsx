8014f650a38336ea03156c192962865ed7c66923
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-08-05 23:59:08 -0300

    Add Container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,33 @@
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 export const Footer = () => {
-    return (
-        <div className="d-flex gap-2 p-2 justify-content-center align-items-center bottom-0 bg-secondary bg-opacity-25 w-100 position-sticky flex-row mt-4 ">
-            <Button variant="outline-light" >
-                <a target="blank" href='https://github.com/marco5x'>    
-                   <img src={'https://raw.githubusercontent.com/seasonfif/github/master/icon/icon_192.png'} alt="GitHub" width='40px' />
-                </a>
-            </Button>
-            <Button variant="outline-light">
-                <a target="blank" href='https://www.linkedin.com/in/marcos-cruz-dev/' >                
-                    <img src={'https://freepngimg.com/thumb/social_media/74290-icons-media-wallpaper-linkedin-desktop-computer-social.png'} alt="Linkedin" width='40px' />
-                </a>
-            </Button>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <div className="d-flex gap-2 p-2 justify-content-center align-items-center bottom-0 bg-secondary bg-opacity-25 position-sticky mt-4 ">
+        <Button variant="outline-light">
+          <a target="blank" href="https://github.com/marco5x">
+            <img
+              src={
+                "https://raw.githubusercontent.com/seasonfif/github/master/icon/icon_192.png"
+              }
+              alt="GitHub"
+              width="40px"
+            />
+          </a>
+        </Button>
+        <Button variant="outline-light">
+          <a target="blank" href="https://www.linkedin.com/in/marcos-cruz-dev/">
+            <img
+              src={
+                "https://freepngimg.com/thumb/social_media/74290-icons-media-wallpaper-linkedin-desktop-computer-social.png"
+              }
+              alt="Linkedin"
+              width="40px"
+            />
+          </a>
+        </Button>
+      </div>
+    </Container>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { MemeCard } from "../components/MemeCard";
 import { getMemes } from "../api/getMemes";
+import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export const Home = () => {
-    const [memes, setMemes] = useState([]);
+  const [memes, setMemes] = useState([]);
 
-    useEffect(() => {
-        getMemes().then((meme) => setMemes(meme.data.memes));
-    }, []);
+  useEffect(() => {
+    getMemes().then((meme) => setMemes(meme.data.memes));
+  }, []);
 
-    return (
-        <>
-            <Row xs={1} s={1} md={2} lg={3} xl={4} className="g-4">
-                {!memes ? (
-                    <h3>Ups 🤦🏻‍♂️</h3>
-                ) : (
-                    memes.map((meme) => (
-                        <Col>
-                            <MemeCard
-                                img={!meme.url ? <Spinner animation="grow" /> : meme.url}
-                                title={meme.name}
-                            />
-                        </Col>
-                    ))
-                )}
-            </Row>
-        </>
-    );
+  return (
+    <Container>
+      <Row xs={1} s={1} md={2} lg={3} xl={4} className="g-3">
+        {!memes ? (
+          <h3>Ups 🤦🏻‍♂️</h3>
+        ) : (
+          memes.map((meme) => (
+            <Col key={meme.name}>
+              <MemeCard
+                img={!meme.url ? <Spinner animation="grow" /> : meme.url}
+                title={meme.name}
+              />
+            </Col>
+          ))
+        )}
+      </Row>
+    </Container>
+  );
 };
