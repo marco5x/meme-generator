519b35bfb7ab1ae1e74ce5ceb5ce26e16913304c
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-08-06 00:34:19 -0300

    Button center in card. image dh-100 in card

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-export const MemeCard = ( props ) => {
-const navigate = useNavigate()
+export const MemeCard = (props) => {
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <Card style={{ width: "18rem", height:"33rem" , margin:"2rem" }}>
-                <Card.Img variant="top" src={props.img}/>
-                <Card.Body>
-                    <Card.Title>{props.title}</Card.Title>
-                    <Button onClick={e => navigate(`/edit?url=${props.img}`)} variant="primary">Editar</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card style={{ width: "18rem", height: "33rem", margin: "2rem" }}>
+        <Card.Img variant="top" src={props.img} className="mh-100" />
+        <Card.Body className="d-flex flex-column align-items-center ">
+          <Card.Title>{props.title}</Card.Title>
+          <Button
+            onClick={(e) => navigate(`/edit?url=${props.img}`)}
+            className="position-absolute bottom-0 mb-1"
+            variant="primary"
+          >
+            Editar
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
